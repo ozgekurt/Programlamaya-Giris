8f79318ee6a7b6a1c058fca35eb107bef99b5121
ozgekurt <32963281+ozgekurt@users.noreply.github.com>
2018-03-05 17:01:32 +0300

    Degiskenleri Yazdıran Kod
    Özge KURT tarafından kodlanmıştır.

---
+++ DegiskenYazdir.java
@@ -0,0 +1,19 @@
+public class DegiskenYazdirma {
+
+  public static void main(String args[]){
+    int sayi=10;
+    float sayi2=25.3f;
+    double pi=3.14;
+    String a="Merhaba";
+    char ch1='o';
+    
+    System.out.println("sayi..: "+sayi);
+    System.out.println("sayi2..: "+sayi2);
+    System.out.println("pi..: " +pi);
+    System.out.println("a..: "+a);
+    System.out.println("ch1..: "+ch1);
+    
+  }
+
+}
+   
